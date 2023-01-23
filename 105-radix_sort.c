bacc05b7d709fb5918541dd5e87d78182ff53d9e
Itsrhoda” <rodahmanyaga.com>
2023-01-23 06:08:02 -0800

    105-radix_sort.c

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -25,7 +25,7 @@ int get_max(int *array, int size)
 }
 
 /**
- * radius_counting_sort - Sort the significant digits of an array of integers
+ * radix_counting_sort - Sort the significant digits of an array of integers
  * in ascending order using the counting sort algorithm.
  * @array: An array of integers.
  * @size: The size of the array.
